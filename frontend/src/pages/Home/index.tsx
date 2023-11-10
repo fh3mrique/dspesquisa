a9b4c1471585ddd5897faaf1695eb458500d356f
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-09 23:05:10 -0300

    Page Home

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import arrow from "../../assets/arrow.svg"
+import GamerImage from "../../assets/gamer.svg"
+
+import "./styles.css"
+
+
+const Home = () => {
+  return (
+    <div className="home-container">
+     
+        <div className="home-text">
+            <h1 className ="home-text-title">
+                Quais jogos a galera mais gosta?
+            </h1>
+            <h3 className="home-text-subtitle">
+                Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
+            </h3>
+            <div className="home-actions">
+                <button className="home-btn">
+                    QUERO SABER QUEM SÃO
+                </button>
+                <div className="home-btn-icon" >
+                    <img src={arrow} alt="" /> 
+                </div>
+            </div>
+        </div>
+
+        <img src={GamerImage} className="home-image"/>
+    </div>
+  )
+}
+
+export default Home
