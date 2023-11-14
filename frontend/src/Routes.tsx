fbe64ffac8a5c0db1af7b9cc6a9ad355c419dfc5
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-11-14 18:05:38 -0300

    Componentizando Filters

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Records from "./pages/Records";
+import Charts from "./pages/Charts";
 
 const Rotas = () => {
   return (
@@ -10,6 +11,7 @@ const Rotas = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/records" element={<Records />} />
+        <Route path="/charts" element={<Charts />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -5,6 +5,7 @@ import { RecordResponse } from "./types"
 import { formatDate } from "./utils"
 import Pagination from "./Pagination"
 import { Link } from "react-router-dom"
+import Filters from "../../components/Filters"
 
 const BASE_URL = "http://localhost:8081"
 
@@ -25,14 +26,8 @@ const Records = () => {
 
   return (
     <div className="page-container">
-      
-      <div className="filters-container records-actions">
-        <Link to="/charts">
-          <button  className="action-filters">
-              VER GRÁFICOS
-          </button>
-        </Link>
-      </div>
+
+      <Filters link="/charts" linkText="VER GRÁFICOS"/>
 
       <table className="records-table" cellPadding="0" cellSpacing="0">
         
